b47e2b4f01195426006b5f3312ea83791122bebe
1ProtonDecay <a.zyabkin@mail.ru>
2024-05-06 23:10:17 +0300

    Initial commit

---
+++ src/MyFirstJavaProgramm.java
@@ -0,0 +1,11 @@
+public class MyFirstJavaProgramm {
+    /*Это моя первая программа на Java. В результате выполнения на экран будет выведено  "Привет, мир"*/
+    public static void main(String[] args) {
+        System.out.println("Привет, мир");
+    }
+
+}
+/*Внутри класса:
+- main: метод
+
+ */
